59b30f4c619bae726eff8341ff1e886e223705fb
BM304 <106486535+BM304@users.noreply.github.com>
2022-06-19 21:54:34 +0700

    Update lesson_7_homework_student4.sql

--- lesson_7_homework_student4.sql
+++ lesson_7_homework_student4.sql
@@ -1,117 +1,102 @@
--- Задание 20: Найдите средний размер hd PC каждого из тех производителей, которые выпускают и принтеры. Вывести: maker, средний размер HD.
-
-select pr.maker, avg(pc.hd) 
-from pc
-join product pr  on pr.model =pc.model  
-where  pr.maker in 
-	(select   pr.maker from product pr
-	 where pr.type='Printer')
-group by pr.maker
-
---схема БД: https://docs.google.com/document/d/1NVORWgdwlKepKq_b8SPRaSpraltxoMg2SIusTEN6mEQ/edit?usp=sharing
 --colab/jupyter: https://colab.research.google.com/drive/1j4XdGIU__NYPVpv74vQa9HUOAkxsgUez?usp=sharing
 
--- Задание 1: Вывести name, class по кораблям, выпущенным после 1920
+--task1  (lesson7)
+-- sqlite3: РЎРґРµР»Р°С‚СЊ С‚РµСЃС‚РѕРІС‹Р№ РїСЂРѕРµРєС‚ СЃ Р‘Р” (sqlite3, project name: task1_7). Р’ С‚Р°Р±Р»РёС†Сѓ table1 Р·Р°РїРёСЃР°С‚СЊ 1000 СЃС‚СЂРѕРє СЃ СЃР»СѓС‡Р°Р№РЅС‹РјРё Р·РЅР°С‡РµРЅРёСЏРјРё (3 РєРѕР»РѕРЅРєРё, С‚РёРї int) РѕС‚ 0 РґРѕ 1000.
+-- Р”Р°Р»РµРµ РїРѕСЃС‚СЂРѕРёС‚СЊ РіРёСЃС‚Р°РіСЂР°РјРјСѓ СЂР°СЃРїСЂРµРґРµР»РµРЅРёСЏ СЌС‚РёС… С‚СЂРµС… РєРѕР»РѕРЅРєРѕ
+request = "CREATE TABLE table1(column1 int, column2 int, column3 int)"
+c.execute (request)
+tables = c.fetchall()
+-- РїСЂРѕРµСЂРєР° РЅР°Р»РёС‡РёСЏ С‚Р°Р±Р»РёС†С‹
+pd.read_sql_query("select * from table1", conn)
+
+-- Р·Р°РїРѕР»РЅРµРЅРёРµ - РЅРµ СЃСЂР°Р±РѕС‚Р°Р»Рѕ((, РІС‹РґР°РІР°Р» СЃРёРЅС‚. РѕС€РёР±РєСѓ, РЅРµ СЂР°Р·РѕР±СЂР°Р»Р°СЃСЊ 
+np.WHILE (i<1001,
+  i=i+1
+  request="INSERT into table1 values (random.randint(0,1000),random.randint(0,1000),random.randint(0,1000))"
+  c.execute (request)
+  tables = c.fetchall()
+ )
+-- РїСЂРѕРµСЂРєР° СЃРѕРґРµСЂР¶РёРјРѕРіРѕ С‚Р°Р±Р»РёС†С‹
+pd.read_sql_query("select * from table1", conn)
+-- РІС‹РІРѕРґР° РЅРµС‚(  
+
+
+
+--task2  (lesson7)
+-- oracle: https://leetcode.com/problems/duplicate-emails/
+select  email 
+from (
+select  email, count( email) as c
+from person 
+group by  email
+) a 
+where c > 1
+/*Р РµР·СѓР»СЊС‚Р°С‚
+Accepted
+Runtime: 348 ms
+Your input
+{"headers": {"Person": ["id", "email"]}, "rows": {"Person": [[1, "a@b.com"], [2, "c@d.com"], [3, "a@b.com"]]}}
+Output
+{"headers": ["EMAIL"], "values": [["a@b.com"]]}
+Expected
+{"headers": ["Email"], "values": [["a@b.com"]]}
+*/
+
+--task3  (lesson7)
+-- oracle: https://leetcode.com/problems/employees-earning-more-than-their-managers/
+select
+  e.name Employee 
+from Employee e
+where e.salary> (select em.salary from Employee em where e.managerId=em.Id)
+/* Р РµР·СѓР»СЊС‚Р°С‚
+Accepted
+Runtime: 327 ms
+Your input
+{"headers": {"Employee": ["id", "name", "salary", "managerId"]}, "rows": {"Employee": [[1, "Joe", 70000, 3], [2, "Henry", 80000, 4], [3, "Sam", 60000, null], [4, "Max", 90000, null]]}}
+Output
+{"headers": ["EMPLOYEE"], "values": [["Joe"]]}
+Expected
+{"headers": ["Employee"], "values": [["Joe"]]}
+*/
+
+
+--task4  (lesson7)
+-- oracle: https://leetcode.com/problems/rank-scores/
+select
+    score,
+    dense_RANK ( ) OVER (order by  score desc) rank
+from Scores
+order by score desc  
+
+/* Р РµР·СѓР»СЊС‚Р°С‚
+Accepted
+Runtime: 346 ms
+Your input
+{"headers": {"Scores": ["id", "score"]}, "rows": {"Scores": [[1, 3.50], [2, 3.65], [3, 4.00], [4, 3.85], [5, 4.00], [6, 3.65]]}}
+Output
+{"headers": ["SCORE", "RANK"], "values": [[4.0, 1], [4.0, 1], [3.85, 2], [3.65, 3], [3.65, 3], [3.5, 4]]}
+Expected
+{"headers": ["score", "rank"], "values": [[4.00, 1], [4.00, 1], [3.85, 2], [3.65, 3], [3.65, 3], [3.50, 4]]}
+*/
+
+
+--task5  (lesson7)
+-- oracle: https://leetcode.com/problems/combine-two-tables/
 select 
-	s.name,
-	s.class 
-from Ships s 
-where s.launched >1920
+    firstName,
+    lastName,
+    city,
+    state 
+from person
+left join address on person.personId = address.personId
 
--- Задание 2: Вывести name, class по кораблям, выпущенным после 1920, но не позднее 1942
-select 
-	s.name,
-	s.class
-from Ships s 
-where (s.launched >1920) and (s.launched <=1942) 
-
--- Задание 3: Какое количество кораблей в каждом классе. Вывести количество и class
-select 
-	s.class,
-	count(s.name)
-from Ships s 
-group by s.class
-
--- Задание 4: Для классов кораблей, калибр орудий которых не менее 16, укажите класс и страну. (таблица classes)
---
-select 
-	cl.class,
-	cl.country 
-from classes cl
-where cl.bore >=16
-
--- Задание 5: Укажите корабли, потопленные в сражениях в Северной Атлантике (таблица Outcomes, North Atlantic). Вывод: ship.
-select 
-	o.ship 
-from Outcomes o
-where o.battle='North Atlantic' and o.result='sunk' 
-
--- Задание 6: Вывести название (ship) последнего потопленного корабля
-select 
-	o.ship
-from Outcomes o
-join Battles b on o.battle = b.name
-where o.result='sunk' and b.date=(
-	select 
-		max(b.date) 
-	from Outcomes o join Battles b on o.battle = b.name 
-	where o.result='sunk')
-	
--- Задание 7: Вывести название корабля (ship) и класс (class) последнего потопленного корабля
-select 
-	o.ship ,
-	s.class
-from Outcomes o
-inner join Battles b on o.battle = b.name
-inner join ships s on o.ship = s.name
-where o.result='sunk' and b.date=(
-	select 
-		max(b.date) 
-	from Outcomes o 
-	inner join Battles b on o.battle = b.name
-	inner join ships s on o.ship = s.name
-	where o.result='sunk')
--- в виду замечания "В отношение Outcomes могут входить корабли, отсутствующие в отношении Ships" выборка проведена среди только существующих в обеих таблицах данных 
-
--- Задание 8: Вывести все потопленные корабли, у которых калибр орудий не менее 16, и которые потоплены. Вывод: ship, class
-select  
-	o.ship ,
-	cl.class
-from outcomes o 
-left join ships s on o.ship = s.name
-left join classes cl on s.class=cl.class
-where o.result='sunk' and cl.bore >=16
--- в виду замечания "В отношение Outcomes могут входить корабли, отсутствующие в отношении Ships" выборка проведена среди только существующих в обеих таблицах данных 
-
-
--- Задание 9: Вывести все классы кораблей, выпущенные США (таблица classes, country = 'USA'). Вывод: class
-select 
-	distinct (class)
-from classes
-where country = 'USA'
-
--- Задание 10: Вывести все корабли, выпущенные США (таблица classes & ships, country = 'USA'). Вывод: name, class
-select 
-	s.name,
-	s.class
-from ships s
-join classes cl on s.class=cl.class
-where cl.country = 'USA'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* Р РµР·СѓР»СЊС‚Р°С‚
+Accepted
+Runtime: 493 ms
+Your input
+{"headers":{"Person":["personId","lastName","firstName"],"Address":["addressId","personId","city","state"]},"rows":{"Person":[[1,"Wang","Allen"],[2,"Alice","Bob"]],"Address":[[1,2,"New York City","New York"],[2,3,"Leetcode","California"]]}}
+Output
+{"headers": ["FIRSTNAME", "LASTNAME", "CITY", "STATE"], "values": [["Bob", "Alice", "New York City", "New York"], ["Allen", "Wang", null, null]]}
+Expected
+{"headers": ["firstName", "lastName", "city", "state"], "values": [["Allen", "Wang", null, null], ["Bob", "Alice", "New York City", "New York"]]}
+*/
